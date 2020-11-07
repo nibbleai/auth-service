8d5ef43250da99daacea84555ffe36bdb8f4f01b
Edouard Theron <ed@edtheron.me>
2020-11-07 15:21:09 -0600

    Refactor business logic

--- nibble_auth_service/app.py
+++ nibble_auth_service/app.py
@@ -1,58 +1,11 @@
 #!/usr/bin/env python
-from collections import defaultdict
-from logging import getLogger
-
 import tornado.ioloop
 import tornado.web
 
-from .base import *
+from .base import AuthHandler, update_config
 
 AUTH_SERVICE_ENDPOINT = '/auth'
 AUTH_SERVICE_PORT = 10001
-
-# Redirect to notebook service by default
-SERVICE_PATH_MAPPING = defaultdict(lambda: '/notebook')
-SERVICE_PATH_MAPPING.update({
-    Service.NOTEBOOK: '/notebook',
-    Service.VSCODE: '/vscode',
-    Service.TERMINAL: '/notebook/terminals/1'
-})
-
-logger = getLogger(__name__)
-
-
-class AuthHandler(tornado.web.RequestHandler):
-    """Main handler to set an auth cookie in exange of a URL query param."""
-    def prepare(self) -> None:
-        self._auth_token = CONFIG.get(TOKEN_CONFIG_KEY, '')
-        self.authenticate()
-
-    def authenticate(self) -> None:
-        token = self.get_argument('token', None, strip=True)
-        if token == self._auth_token:
-            logger.info("Authentication succeeded.")
-            return
-        logger.error("Authentication failed.")
-        raise tornado.web.HTTPError(403)
-
-    def get(self) -> None:
-        # At this point, an unauthorized user would already be rejected.
-        cookie = self.get_cookie(COOKIE_NAME)
-        if cookie != self._auth_token:
-            # This browser either does not have the authentication cookie set,
-            # or has an old one. In both cases, it needs a fresh cookie.
-            cookie_data = build_cookie(self.request.host, self._auth_token)
-            self.set_cookie(**cookie_data)
-
-        service = self.get_argument('service', Service.NOTEBOOK).lower()
-        redirect_to = SERVICE_PATH_MAPPING[service]
-
-        folder = self.get_argument('folder', '')
-        suffix = get_path_suffix(service, folder)
-        redirect_to += suffix
-
-        logger.info("Redirecting...")
-        self.redirect(redirect_to, permanent=False)
 
 
 def main():
--- nibble_auth_service/base.py
+++ nibble_auth_service/base.py
@@ -1,23 +1,64 @@
 import json
+from logging import getLogger
 import time
+
+import tornado.web
+
+logger = getLogger(__name__)
 
 COOKIE_NAME = 'nibble_auth_token'
 COOKIE_LIFETIME = 3600 * 24 * 7  # 7 days
 
-WORKDIR_CONFIG_KEY = 'working_dir'
-TOKEN_CONFIG_KEY = 'token'
-
 CONFIG_LOCATION = '/etc/nibble/config.json'
 CONFIG = {}  # to be updated at runtime
 
+WORKDIR_CONFIG_KEY = 'working_dir'
+TOKEN_CONFIG_KEY = 'token'
+
 # If the expected folder is not found, redirect users to home directory
 FALLBACK_LOCATION = '/home'
+
+__all__ = ['AuthHandler', 'update_config']
 
 
 class Service:
     VSCODE = 'vscode'
     NOTEBOOK = 'notebook'
     TERMINAL = 'terminal'
+
+
+class AuthHandler(tornado.web.RequestHandler):
+    """Main handler to set an auth cookie in exange of a URL query param."""
+    def prepare(self) -> None:
+        self._auth_token = CONFIG.get(TOKEN_CONFIG_KEY, '')
+        self.authenticate()
+
+    def authenticate(self) -> None:
+        token = self.get_argument('token', None, strip=True)
+        if token == self._auth_token:
+            logger.info("Authentication succeeded.")
+            return
+        logger.error("Authentication failed.")
+        raise tornado.web.HTTPError(403)
+
+    def get(self) -> None:
+        # At this point, an unauthorized user would already be rejected.
+        cookie = self.get_cookie(COOKIE_NAME)
+        if cookie != self._auth_token:
+            # This browser either does not have the authentication cookie set,
+            # or has an old one. In both cases, it needs a fresh cookie.
+            cookie_data = build_cookie(self.request.host, self._auth_token)
+            self.set_cookie(**cookie_data)
+
+        service = self.get_argument('service', Service.NOTEBOOK).lower()
+        folder = self.get_argument('folder', '')
+
+        redirect_to = get_redirection_path(service)
+        suffix = get_path_suffix(service, folder)
+        redirect_to += suffix
+
+        logger.info("Redirecting...")
+        self.redirect(redirect_to, permanent=False)
 
 
 def build_cookie(host: str, token: str) -> dict:
@@ -31,15 +72,12 @@ def build_cookie(host: str, token: str) -> dict:
     }
 
 
-def update_config():
-    global CONFIG
-    try:
-        with open(CONFIG_LOCATION, 'rb') as fp:
-            runtime_config = json.load(fp)
-    except FileNotFoundError:
-        return
-
-    CONFIG.update(runtime_config)
+def get_redirection_path(service: str) -> str:
+    if service == Service.VSCODE:
+        return '/vscode'
+    if service == Service.TERMINAL:
+        return '/notebook/terminals/1'
+    return '/notebook'  # redirect to notebook service by default
 
 
 def get_path_suffix(service: str, folder: str) -> str:
@@ -56,3 +94,14 @@ def get_path_suffix(service: str, folder: str) -> str:
         return f'/tree/{folder}'
 
     return ''
+
+
+def update_config() -> None:
+    global CONFIG
+    try:
+        with open(CONFIG_LOCATION, 'rb') as fp:
+            runtime_config = json.load(fp)
+    except FileNotFoundError:
+        return
+
+    CONFIG.update(runtime_config)
